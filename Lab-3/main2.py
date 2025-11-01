ba4f015fded0eb9617f505c74f5e19806b40c223
MariusGhiniei <marius_iulian_23@yahoo.com>
2025-11-01 17:17:16 +0200

    H3 - ex b -> facePhoto mask created

--- Lab-3/main2.py
+++ Lab-3/main2.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+
+## TO DO: pentru fiecare dintre cele 2 foldere cu imagini(face and family) trebuie sa calculez maska de pixeli folosind
+# fiecare metoda RGB, HSV, YCbCr si le compar cu mask - urile din Ground_Truth apoi sa fac matriciele TP,FN,FP,TN si
+# la final accuratetea.
+
 def rgbPixelMethod(R, G, B):
     R = int(R)
     G = int(G)
@@ -27,9 +32,72 @@ def yCbCrPixelMethod(R,G,B):
 
     return Y > 80 and Cb > 85 and Cb < 135 and Cr > 135 and Cr < 180
 
-## TO DO: pentru fiecare dintre cele 2 foldere cu imagini(face and family) trebuie sa calculez maska de pixeli folosind fiecare metoda
-    # RGB, HSV, YCbCr si le compar cu mask - urile din Ground_Truth apoi sa fac matriciele TP,FN,FP,TN si la final accuratetea.
+dataset = "/Users/marius/PycharmProjects/CV-lab1/Lab-3/Face_Dataset"
 
+# Face photo
+facePhotoDataset = os.path.join(dataset,"Pratheepan_Dataset/FacePhoto")
+familyPhotoDataset = os.path.join(dataset, "Pratheepan_Dataset/FamilyPhoto")
+
+facePhotoDatasetMasks = "/Users/marius/PycharmProjects/CV-lab1/Lab-3/Face_Dataset/Pratheepan_Dataset/facePhotoMasks"
+familyPhotoDatasetMasks = "/Users/marius/PycharmProjects/CV-lab1/Lab-3/Face_Dataset/Pratheepan_Dataset/familyPhotoMasks"
+
+for image in os.listdir(facePhotoDataset):
+    if image is None:
+        raise ValueError("Cannot read image: " + image)
+    elif image.endswith(".jpg") or image.endswith(".jpeg"):
+        imagePath = os.path.join(facePhotoDataset, image)
+
+        # RGB - method
+        img = cv2.imread(imagePath, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        h, w = img.shape[:2]
+        mask = np.zeros((h, w), dtype=np.uint8)
+
+        for y in range(h):
+            for x in range(w):
+                r,g,b = img[y,x]
+                if rgbPixelMethod(r,g,b):
+                    mask[y,x] = 255
+                else: mask[y,x] = 0
+
+        name = os.path.splitext(image)[0] + "_maskRGB.png"
+        outputPath = os.path.join(facePhotoDatasetMasks, name)
+        cv2.imwrite(outputPath, mask)
+
+        # HSV - method
+
+        h, w = img.shape[:2]
+        mask = np.zeros((h, w), dtype=np.uint8)
+
+        for y in range(h):
+            for x in range(w):
+                r, g, b = img[y, x]
+                if hsvPixelMethod(r, g, b):
+                    mask[y, x] = 255
+                else:
+                    mask[y, x] = 0
+
+        name = os.path.splitext(image)[0] + "_maskHSV.png"
+        outputPath = os.path.join(facePhotoDatasetMasks, name)
+        cv2.imwrite(outputPath, mask)
+
+        #YCbCr - method
+
+        h, w = img.shape[:2]
+        mask = np.zeros((h, w), dtype=np.uint8)
+
+        for y in range(h):
+            for x in range(w):
+                r, g, b = img[y, x]
+                if yCbCrPixelMethod(r, g, b):
+                    mask[y, x] = 255
+                else:
+                    mask[y, x] = 0
+
+        name = os.path.splitext(image)[0] + "_maskYCbCr.png"
+        outputPath = os.path.join(facePhotoDatasetMasks, name)
+        cv2.imwrite(outputPath, mask)
 
 
 
